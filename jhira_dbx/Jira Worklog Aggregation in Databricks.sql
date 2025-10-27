4abfc8d36784a5e6ebee7dd0aa1ce449c81f4115
Serhiy Dranko <163868717+serhiy30@users.noreply.github.com>
2025-10-27 20:52:44 +0100

    Update Jira Worklog Aggregation in Databricks.sql

--- jhira_dbx/Jira Worklog Aggregation in Databricks.sql
+++ jhira_dbx/Jira Worklog Aggregation in Databricks.sql
@@ -4,171 +4,169 @@
 -- Written for Databricks (Spark SQL)
 -- ===========================================
 
-# Databricks notebook source
-# MAGIC %sql
-# MAGIC CREATE OR REPLACE TABLE main.data.jira.gold.taskdata
-# MAGIC SELECT
-# MAGIC  Tempo.Year,
-# MAGIC  Tempo.Month,
-# MAGIC  Tempo.TEMPO_Author_ID,
-# MAGIC  Tempo.ASSIGNEE, 
-# MAGIC  Tempo.Epic_ID,
-# MAGIC  Tempo.Epic_SUMMARY,
-# MAGIC  Tempo.Task_ID,
-# MAGIC  Tempo.Task_SUMMARY,
-# MAGIC  Tempo.Sub_task_ID,
-# MAGIC  Tempo.Sub_task_SUMMARY,
-# MAGIC  SUM(Tempo.TIMESPENTHRS) AS Hours_spent,
-# MAGIC  SUM(Tempo.TIMESPENTSECONDS) AS Seconds_spent,
-# MAGIC  date_format(Tempo.START_DATE, 'yyyy-MM-dd') AS START_DATE,
-# MAGIC  CASE 
-# MAGIC     WHEN (Tempo.Expense_Type_3 IS NULL OR Tempo.Expense_Type_3 = '') 
-# MAGIC          AND (Tempo.Expense_Type_2 IS NULL OR Tempo.Expense_Type_2 = '') 
-# MAGIC          THEN Tempo.Expense_Type_1
-# MAGIC     WHEN (Tempo.Expense_Type_3 IS NULL OR Tempo.Expense_Type_3 = '') 
-# MAGIC          AND (Tempo.Expense_Type_2 IS NOT NULL AND Tempo.Expense_Type_2 != '') 
-# MAGIC          THEN Tempo.Expense_Type_2
-# MAGIC     ELSE Tempo.Expense_Type_3 
-# MAGIC  END AS Expense_Type
-# MAGIC
-# MAGIC FROM
-# MAGIC (Select 
-# MAGIC     month(tmp.STARTDATE) AS Month,
-# MAGIC     year(tmp.STARTDATE) AS Year,
-# MAGIC     tmp.STARTDATE AS START_DATE,
-# MAGIC     CASE WHEN u.DISPLAYNAME IS null THEN 'Unassigned' ELSE u.DISPLAYNAME END  AS TEMPO_Author_ID, 
-# MAGIC     CASE WHEN ji.ASSIGNEE IS null THEN 'Unassigned' ELSE ji.ASSIGNEE END AS ASSIGNEE, 
-# MAGIC     tmp.ISSUEID,
-# MAGIC     ji.IO_JIRA_ACCOUNT AS Expense_Type_1,
-# MAGIC     ji.PROJECT_KEY AS PARENT,
-# MAGIC     log.ISSUEKEY AS Epic_ID,
-# MAGIC     ji.SUMMARY AS Epic_SUMMARY,
-# MAGIC     ji.ISSUETYPE,
-# MAGIC     '' AS Task_ID,
-# MAGIC     '' AS Task_SUMMARY,
-# MAGIC     '' AS Expense_Type_2,
-# MAGIC     '' AS Sub_task_ID,
-# MAGIC     '' AS Sub_task_SUMMARY,
-# MAGIC     '' AS Expense_Type_3,
-# MAGIC     tmp.DESCRIPTION,
-# MAGIC     tmp.TIMESPENTSECONDS / 3600 AS TIMESPENTHRS,
-# MAGIC     tmp.TIMESPENTSECONDS AS TIMESPENTSECONDS,
-# MAGIC     tmp.AUTHORACCOUNTID,
-# MAGIC     tmp.TEMPOWORKLOGID
-# MAGIC     
-# MAGIC FROM main.data.jira.silver.data.tempo_worklogs tmp
-# MAGIC     LEFT JOIN main.data.jira.silver.data.users u ON tmp.AUTHORACCOUNTID = u.ACCOUNTID
-# MAGIC     LEFT JOIN main.data.jira.silver.data.change_logs log ON log.ISSUEID = tmp.ISSUEID
-# MAGIC     LEFT JOIN main.data.jira.silver.data.jira_issues ji  ON ji.KEY = log.ISSUEKEY
-# MAGIC WHERE  
-# MAGIC    ji.ISSUETYPE IN (SELECT ISSUETYPE_NAME FROM main.data.jira.silver.data.types_for_reporting WHERE DBX_report_column = 'Project/Epic')
-# MAGIC    AND tmp.DELETEDAT IS NULL and tmp.isdeleted = false
-# MAGIC
-# MAGIC GROUP BY Month, Year, START_DATE, TEMPO_Author_ID, ji.ASSIGNEE, tmp.ISSUEID, ji.PROJECT_KEY, Epic_ID, Epic_SUMMARY, ji.ISSUETYPE, tmp.AUTHORACCOUNTID, TIMESPENTHRS, 
-# MAGIC  TIMESPENTSECONDS, tmp.DESCRIPTION, tmp.TEMPOWORKLOGID, Expense_Type_1, Expense_Type_2, Expense_Type_3
-# MAGIC
-# MAGIC UNION ALL 
-# MAGIC
-# MAGIC Select 
-# MAGIC     month(tmp.STARTDATE) AS Month,
-# MAGIC     year(tmp.STARTDATE) AS Year,
-# MAGIC    tmp.STARTDATE AS START_DATE,
-# MAGIC     CASE WHEN u.DISPLAYNAME IS null THEN 'Unassigned' ELSE u.DISPLAYNAME END  AS TEMPO_Author_ID, 
-# MAGIC     CASE WHEN ji.ASSIGNEE IS null THEN 'Unassigned' ELSE ji.ASSIGNEE END AS ASSIGNEE, 
-# MAGIC     tmp.ISSUEID,
-# MAGIC      jit.IO_JIRA_ACCOUNT AS Expense_Type_1,
-# MAGIC     CASE WHEN jit.ISSUETYPE IS NULL THEN 'Epic'ELSE jit.ISSUETYPE END AS PARENT,
-# MAGIC     CASE WHEN ji.PARENT IS NULL THEN ji.PROJECT_KEY ELSE ji.PARENT END AS Epic_ID,
-# MAGIC     CASE WHEN jit.SUMMARY IS NULL THEN proj.NAME ELSE jit.SUMMARY END AS Epic_SUMMARY,
-# MAGIC     ji.ISSUETYPE,
-# MAGIC     log.ISSUEKEY AS Task_ID,
-# MAGIC     ji.SUMMARY AS Task_SUMMARY,
-# MAGIC     ji.IO_JIRA_ACCOUNT AS Expense_Type_2,
-# MAGIC     '' AS Sub_task_ID,
-# MAGIC     '' AS Sub_task_SUMMARY,
-# MAGIC     '' AS Expense_Type_3,
-# MAGIC     tmp.DESCRIPTION,
-# MAGIC     tmp.TIMESPENTSECONDS / 3600 AS TIMESPENTHRS,
-# MAGIC     tmp.TIMESPENTSECONDS AS TIMESPENTSECONDS,
-# MAGIC     tmp.AUTHORACCOUNTID,
-# MAGIC     tmp.TEMPOWORKLOGID
-# MAGIC FROM
-# MAGIC    main.data.jira.silver.data.tempo_worklogs tmp
-# MAGIC  LEFT JOIN main.data.jira.silver.data.users u ON tmp.AUTHORACCOUNTID = u.ACCOUNTID
-# MAGIC  LEFT JOIN main.data.jira.silver.data.change_logs log ON log.ISSUEID = tmp.ISSUEID
-# MAGIC  LEFT JOIN main.data.jira.silver.data.jira_issues ji  ON ji.KEY = log.ISSUEKEY
-# MAGIC  LEFT JOIN main.data.jira.silver.data.jira_issues jit  ON jit.KEY = ji.PARENT
-# MAGIC  LEFT JOIN main.data.jira.silver.data.jira_issues jie  ON jie.KEY = jit.PARENT
-# MAGIC  LEFT JOIN (SELECT PROJECTKEY, MAX(NAME) AS NAME FROM main.data.jira.silver.data.projects Group by all) AS proj ON ji.PROJECT_KEY = proj.PROJECTKEY
-# MAGIC WHERE  
-# MAGIC    ji.ISSUETYPE IN (SELECT ISSUETYPE_NAME FROM main.data.jira.silver.data.types_for_reporting WHERE DBX_report_column IN ('Task/Story', 'Other'))
-# MAGIC    AND tmp.DELETEDAT IS NULL and tmp.isdeleted = false
-# MAGIC  
-# MAGIC GROUP BY Month, Year, START_DATE, TEMPO_Author_ID, ji.ASSIGNEE,tmp.ISSUEID, CASE WHEN jit.ISSUETYPE IS NULL THEN 'Epic'ELSE jit.ISSUETYPE END, Epic_ID, Epic_SUMMARY, 
-# MAGIC  ji.ISSUETYPE, Task_ID, Task_SUMMARY, tmp.AUTHORACCOUNTID, TIMESPENTHRS, TIMESPENTSECONDS, tmp.DESCRIPTION, tmp.TEMPOWORKLOGID, Expense_Type_1, Expense_Type_2, Expense_Type_3
-# MAGIC
-# MAGIC UNION ALL
-# MAGIC
-# MAGIC Select 
-# MAGIC     month(tmp.STARTDATE) AS Month,
-# MAGIC     year(tmp.STARTDATE) AS Year,
-# MAGIC     tmp.STARTDATE AS START_DATE,
-# MAGIC     CASE WHEN u.DISPLAYNAME IS null THEN 'Unassigned' ELSE u.DISPLAYNAME END  AS TEMPO_Author_ID, 
-# MAGIC     CASE WHEN ji.ASSIGNEE IS null THEN 'Unassigned' ELSE ji.ASSIGNEE END AS ASSIGNEE, 
-# MAGIC     tmp.ISSUEID,
-# MAGIC     jie.IO_JIRA_ACCOUNT AS Expense_Type_1,
-# MAGIC     jit.ISSUETYPE AS PARENT,
-# MAGIC     CASE WHEN jit.PARENT IS NULL THEN jit.PROJECT_KEY ELSE jit.PARENT END AS Epic_ID,
-# MAGIC     CASE WHEN jie.SUMMARY IS NULL THEN jit.PROJECT ELSE jie.SUMMARY END AS Epic_SUMMARY,
-# MAGIC     ji.ISSUETYPE,
-# MAGIC     CASE WHEN ji.PARENT IS NULL THEN ji.PROJECT_KEY ELSE ji.PARENT END AS Task_ID,
-# MAGIC     jit.SUMMARY AS Task_SUMMARY,
-# MAGIC     jit.IO_JIRA_ACCOUNT AS Expense_Type_2,
-# MAGIC     log.ISSUEKEY AS Sub_task_ID,
-# MAGIC     ji.SUMMARY AS Sub_task_SUMMARY,
-# MAGIC     ji.IO_JIRA_ACCOUNT AS Expense_Type_3,
-# MAGIC     tmp.DESCRIPTION,
-# MAGIC     tmp.TIMESPENTSECONDS / 3600 AS TIMESPENTHRS,
-# MAGIC     tmp.TIMESPENTSECONDS AS TIMESPENTSECONDS,
-# MAGIC     tmp.AUTHORACCOUNTID,
-# MAGIC     tmp.TEMPOWORKLOGID
-# MAGIC
-# MAGIC FROM
-# MAGIC    main.data.jira.silver.data.tempo_worklogs tmp
-# MAGIC    LEFT JOIN main.data.jira.silver.data.users u ON tmp.AUTHORACCOUNTID = u.ACCOUNTID
-# MAGIC    LEFT JOIN main.data.jira.silver.data.change_logs log ON log.ISSUEID = tmp.ISSUEID
-# MAGIC    LEFT JOIN main.data.jira.silver.data.jira_issues ji  ON ji.KEY = log.ISSUEKEY
-# MAGIC    LEFT JOIN main.data.jira.silver.data.jira_issues jit  ON jit.KEY = ji.PARENT
-# MAGIC    LEFT JOIN main.data.jira.silver.data.jira_issues jie  ON jie.KEY = jit.PARENT
-# MAGIC WHERE  
-# MAGIC    ji.ISSUETYPE IN (SELECT ISSUETYPE_NAME FROM main.data.jira.silver.data.types_for_reporting WHERE DBX_report_column IN ('Sub-Task'))
-# MAGIC    AND tmp.DELETEDAT IS NULL and tmp.isdeleted = false
-# MAGIC    
-# MAGIC GROUP BY Month, Year, START_DATE, TEMPO_Author_ID, ji.ASSIGNEE, tmp.ISSUEID, jit.ISSUETYPE, Epic_ID, Epic_SUMMARY, ji.ISSUETYPE, Task_ID, Task_SUMMARY, Sub_task_ID, 
-# MAGIC  Sub_task_SUMMARY, tmp.AUTHORACCOUNTID, TIMESPENTHRS, TIMESPENTSECONDS, tmp.DESCRIPTION, tmp.TEMPOWORKLOGID, Expense_Type_1, Expense_Type_2, Expense_Type_3
-# MAGIC ORDER BY TEMPO_Author_ID) As Tempo
-# MAGIC
-# MAGIC
-# MAGIC GROUP BY 
-# MAGIC  Tempo.Month,
-# MAGIC  Tempo.Year,
-# MAGIC  START_DATE,
-# MAGIC  Tempo.TEMPO_Author_ID, 
-# MAGIC  Tempo.ASSIGNEE, 
-# MAGIC  Tempo.Epic_ID,
-# MAGIC  Tempo.Epic_SUMMARY,
-# MAGIC  Tempo.Task_ID,
-# MAGIC  Tempo.Task_SUMMARY,
-# MAGIC  Tempo.Sub_task_ID,
-# MAGIC  Tempo.Sub_task_SUMMARY,
-# MAGIC   CASE 
-# MAGIC     WHEN (Tempo.Expense_Type_3 IS NULL OR Tempo.Expense_Type_3 = '') 
-# MAGIC          AND (Tempo.Expense_Type_2 IS NULL OR Tempo.Expense_Type_2 = '') 
-# MAGIC          THEN Tempo.Expense_Type_1
-# MAGIC     WHEN (Tempo.Expense_Type_3 IS NULL OR Tempo.Expense_Type_3 = '') 
-# MAGIC          AND (Tempo.Expense_Type_2 IS NOT NULL AND Tempo.Expense_Type_2 != '') 
-# MAGIC          THEN Tempo.Expense_Type_2
-# MAGIC     ELSE Tempo.Expense_Type_3 
-# MAGIC     END 
-# MAGIC
-# MAGIC Order by Year Desc, Month Desc, Tempo.ASSIGNEE, Tempo.Epic_ID, Tempo.Task_ID, Tempo.Sub_task_ID;
+
+SELECT
+ Tempo.Year,
+ Tempo.Month,
+ Tempo.TEMPO_Author_ID,
+ Tempo.ASSIGNEE, 
+ Tempo.Epic_ID,
+ Tempo.Epic_SUMMARY,
+ Tempo.Task_ID,
+ Tempo.Task_SUMMARY,
+ Tempo.Sub_task_ID,
+ Tempo.Sub_task_SUMMARY,
+ SUM(Tempo.TIMESPENTHRS) AS Hours_spent,
+ SUM(Tempo.TIMESPENTSECONDS) AS Seconds_spent,
+ date_format(Tempo.START_DATE, 'yyyy-MM-dd') AS START_DATE,
+ CASE 
+    WHEN (Tempo.Expense_Type_3 IS NULL OR Tempo.Expense_Type_3 = '') 
+         AND (Tempo.Expense_Type_2 IS NULL OR Tempo.Expense_Type_2 = '') 
+         THEN Tempo.Expense_Type_1
+    WHEN (Tempo.Expense_Type_3 IS NULL OR Tempo.Expense_Type_3 = '') 
+         AND (Tempo.Expense_Type_2 IS NOT NULL AND Tempo.Expense_Type_2 != '') 
+         THEN Tempo.Expense_Type_2
+    ELSE Tempo.Expense_Type_3 
+ END AS Expense_Type
+
+FROM
+(Select 
+    month(tmp.STARTDATE) AS Month,
+    year(tmp.STARTDATE) AS Year,
+    tmp.STARTDATE AS START_DATE,
+    CASE WHEN u.DISPLAYNAME IS null THEN 'Unassigned' ELSE u.DISPLAYNAME END  AS TEMPO_Author_ID, 
+    CASE WHEN ji.ASSIGNEE IS null THEN 'Unassigned' ELSE ji.ASSIGNEE END AS ASSIGNEE, 
+    tmp.ISSUEID,
+    ji.IO_JIRA_ACCOUNT AS Expense_Type_1,
+    ji.PROJECT_KEY AS PARENT,
+    log.ISSUEKEY AS Epic_ID,
+    ji.SUMMARY AS Epic_SUMMARY,
+    ji.ISSUETYPE,
+    '' AS Task_ID,
+    '' AS Task_SUMMARY,
+    '' AS Expense_Type_2,
+    '' AS Sub_task_ID,
+    '' AS Sub_task_SUMMARY,
+    '' AS Expense_Type_3,
+    tmp.DESCRIPTION,
+    tmp.TIMESPENTSECONDS / 3600 AS TIMESPENTHRS,
+    tmp.TIMESPENTSECONDS AS TIMESPENTSECONDS,
+    tmp.AUTHORACCOUNTID,
+    tmp.TEMPOWORKLOGID
+    
+FROM main.data.jira.silver.data.tempo_worklogs tmp
+    LEFT JOIN main.data.jira.silver.data.users u ON tmp.AUTHORACCOUNTID = u.ACCOUNTID
+    LEFT JOIN main.data.jira.silver.data.change_logs log ON log.ISSUEID = tmp.ISSUEID
+    LEFT JOIN main.data.jira.silver.data.jira_issues ji  ON ji.KEY = log.ISSUEKEY
+WHERE  
+   ji.ISSUETYPE IN (SELECT ISSUETYPE_NAME FROM main.data.jira.silver.data.types_for_reporting WHERE DBX_report_column = 'Project/Epic')
+   AND tmp.DELETEDAT IS NULL and tmp.isdeleted = false
+
+GROUP BY Month, Year, START_DATE, TEMPO_Author_ID, ji.ASSIGNEE, tmp.ISSUEID, ji.PROJECT_KEY, Epic_ID, Epic_SUMMARY, ji.ISSUETYPE, tmp.AUTHORACCOUNTID, TIMESPENTHRS, 
+ TIMESPENTSECONDS, tmp.DESCRIPTION, tmp.TEMPOWORKLOGID, Expense_Type_1, Expense_Type_2, Expense_Type_3
+
+UNION ALL 
+
+Select 
+    month(tmp.STARTDATE) AS Month,
+    year(tmp.STARTDATE) AS Year,
+   tmp.STARTDATE AS START_DATE,
+    CASE WHEN u.DISPLAYNAME IS null THEN 'Unassigned' ELSE u.DISPLAYNAME END  AS TEMPO_Author_ID, 
+    CASE WHEN ji.ASSIGNEE IS null THEN 'Unassigned' ELSE ji.ASSIGNEE END AS ASSIGNEE, 
+    tmp.ISSUEID,
+     jit.IO_JIRA_ACCOUNT AS Expense_Type_1,
+    CASE WHEN jit.ISSUETYPE IS NULL THEN 'Epic'ELSE jit.ISSUETYPE END AS PARENT,
+    CASE WHEN ji.PARENT IS NULL THEN ji.PROJECT_KEY ELSE ji.PARENT END AS Epic_ID,
+    CASE WHEN jit.SUMMARY IS NULL THEN proj.NAME ELSE jit.SUMMARY END AS Epic_SUMMARY,
+    ji.ISSUETYPE,
+    log.ISSUEKEY AS Task_ID,
+    ji.SUMMARY AS Task_SUMMARY,
+    ji.IO_JIRA_ACCOUNT AS Expense_Type_2,
+    '' AS Sub_task_ID,
+    '' AS Sub_task_SUMMARY,
+    '' AS Expense_Type_3,
+    tmp.DESCRIPTION,
+    tmp.TIMESPENTSECONDS / 3600 AS TIMESPENTHRS,
+    tmp.TIMESPENTSECONDS AS TIMESPENTSECONDS,
+    tmp.AUTHORACCOUNTID,
+    tmp.TEMPOWORKLOGID
+FROM
+   main.data.jira.silver.data.tempo_worklogs tmp
+ LEFT JOIN main.data.jira.silver.data.users u ON tmp.AUTHORACCOUNTID = u.ACCOUNTID
+ LEFT JOIN main.data.jira.silver.data.change_logs log ON log.ISSUEID = tmp.ISSUEID
+ LEFT JOIN main.data.jira.silver.data.jira_issues ji  ON ji.KEY = log.ISSUEKEY
+ LEFT JOIN main.data.jira.silver.data.jira_issues jit  ON jit.KEY = ji.PARENT
+ LEFT JOIN main.data.jira.silver.data.jira_issues jie  ON jie.KEY = jit.PARENT
+ LEFT JOIN (SELECT PROJECTKEY, MAX(NAME) AS NAME FROM main.data.jira.silver.data.projects Group by all) AS proj ON ji.PROJECT_KEY = proj.PROJECTKEY
+WHERE  
+   ji.ISSUETYPE IN (SELECT ISSUETYPE_NAME FROM main.data.jira.silver.data.types_for_reporting WHERE DBX_report_column IN ('Task/Story', 'Other'))
+   AND tmp.DELETEDAT IS NULL and tmp.isdeleted = false
+ 
+GROUP BY Month, Year, START_DATE, TEMPO_Author_ID, ji.ASSIGNEE,tmp.ISSUEID, CASE WHEN jit.ISSUETYPE IS NULL THEN 'Epic'ELSE jit.ISSUETYPE END, Epic_ID, Epic_SUMMARY, 
+ ji.ISSUETYPE, Task_ID, Task_SUMMARY, tmp.AUTHORACCOUNTID, TIMESPENTHRS, TIMESPENTSECONDS, tmp.DESCRIPTION, tmp.TEMPOWORKLOGID, Expense_Type_1, Expense_Type_2, Expense_Type_3
+
+UNION ALL
+
+Select 
+    month(tmp.STARTDATE) AS Month,
+    year(tmp.STARTDATE) AS Year,
+    tmp.STARTDATE AS START_DATE,
+    CASE WHEN u.DISPLAYNAME IS null THEN 'Unassigned' ELSE u.DISPLAYNAME END  AS TEMPO_Author_ID, 
+    CASE WHEN ji.ASSIGNEE IS null THEN 'Unassigned' ELSE ji.ASSIGNEE END AS ASSIGNEE, 
+    tmp.ISSUEID,
+    jie.IO_JIRA_ACCOUNT AS Expense_Type_1,
+    jit.ISSUETYPE AS PARENT,
+    CASE WHEN jit.PARENT IS NULL THEN jit.PROJECT_KEY ELSE jit.PARENT END AS Epic_ID,
+    CASE WHEN jie.SUMMARY IS NULL THEN jit.PROJECT ELSE jie.SUMMARY END AS Epic_SUMMARY,
+    ji.ISSUETYPE,
+    CASE WHEN ji.PARENT IS NULL THEN ji.PROJECT_KEY ELSE ji.PARENT END AS Task_ID,
+    jit.SUMMARY AS Task_SUMMARY,
+    jit.IO_JIRA_ACCOUNT AS Expense_Type_2,
+    log.ISSUEKEY AS Sub_task_ID,
+    ji.SUMMARY AS Sub_task_SUMMARY,
+    ji.IO_JIRA_ACCOUNT AS Expense_Type_3,
+    tmp.DESCRIPTION,
+    tmp.TIMESPENTSECONDS / 3600 AS TIMESPENTHRS,
+    tmp.TIMESPENTSECONDS AS TIMESPENTSECONDS,
+    tmp.AUTHORACCOUNTID,
+    tmp.TEMPOWORKLOGID
+
+FROM
+   main.data.jira.silver.data.tempo_worklogs tmp
+   LEFT JOIN main.data.jira.silver.data.users u ON tmp.AUTHORACCOUNTID = u.ACCOUNTID
+   LEFT JOIN main.data.jira.silver.data.change_logs log ON log.ISSUEID = tmp.ISSUEID
+   LEFT JOIN main.data.jira.silver.data.jira_issues ji  ON ji.KEY = log.ISSUEKEY
+   LEFT JOIN main.data.jira.silver.data.jira_issues jit  ON jit.KEY = ji.PARENT
+   LEFT JOIN main.data.jira.silver.data.jira_issues jie  ON jie.KEY = jit.PARENT
+WHERE  
+   ji.ISSUETYPE IN (SELECT ISSUETYPE_NAME FROM main.data.jira.silver.data.types_for_reporting WHERE DBX_report_column IN ('Sub-Task'))
+   AND tmp.DELETEDAT IS NULL and tmp.isdeleted = false
+   
+GROUP BY Month, Year, START_DATE, TEMPO_Author_ID, ji.ASSIGNEE, tmp.ISSUEID, jit.ISSUETYPE, Epic_ID, Epic_SUMMARY, ji.ISSUETYPE, Task_ID, Task_SUMMARY, Sub_task_ID, 
+ Sub_task_SUMMARY, tmp.AUTHORACCOUNTID, TIMESPENTHRS, TIMESPENTSECONDS, tmp.DESCRIPTION, tmp.TEMPOWORKLOGID, Expense_Type_1, Expense_Type_2, Expense_Type_3
+ORDER BY TEMPO_Author_ID) As Tempo
+
+
+GROUP BY 
+ Tempo.Month,
+ Tempo.Year,
+ START_DATE,
+ Tempo.TEMPO_Author_ID, 
+ Tempo.ASSIGNEE, 
+ Tempo.Epic_ID,
+ Tempo.Epic_SUMMARY,
+ Tempo.Task_ID,
+ Tempo.Task_SUMMARY,
+ Tempo.Sub_task_ID,
+ Tempo.Sub_task_SUMMARY,
+  CASE 
+    WHEN (Tempo.Expense_Type_3 IS NULL OR Tempo.Expense_Type_3 = '') 
+         AND (Tempo.Expense_Type_2 IS NULL OR Tempo.Expense_Type_2 = '') 
+         THEN Tempo.Expense_Type_1
+    WHEN (Tempo.Expense_Type_3 IS NULL OR Tempo.Expense_Type_3 = '') 
+         AND (Tempo.Expense_Type_2 IS NOT NULL AND Tempo.Expense_Type_2 != '') 
+         THEN Tempo.Expense_Type_2
+    ELSE Tempo.Expense_Type_3 
+    END 
+
+Order by Year Desc, Month Desc, Tempo.ASSIGNEE, Tempo.Epic_ID, Tempo.Task_ID, Tempo.Sub_task_ID;
